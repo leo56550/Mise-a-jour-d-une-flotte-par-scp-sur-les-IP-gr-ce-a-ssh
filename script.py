ba1eea5c8564e8bd754724304aac3c2573c18ca5
leo56550 <67288623+leo56550@users.noreply.github.com>
2022-11-05 23:29:49 +0100

    Update script.py

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ import filecmp
 def compareandcpy(ip) : 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ip,username='root',key_filename='holotov')
+    ssh.connect(ip,username='root',key_filename='name')
     scp = SCPClient(ssh.get_transport())
     stdin, stdout, stderr = ssh.exec_command('ls')
     scp.get('start.sh','startsh.remote')
